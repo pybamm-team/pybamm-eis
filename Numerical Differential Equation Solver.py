b148d65b723d278cb0eca25737062162d9982373
rish31415 <rish31415@gmail.com>
2022-07-14 10:30:25 +0100

    Added Conjugate Gradient method

--- Numerical Differential Equation Solver.py
+++ Numerical Differential Equation Solver.py
@@ -9,27 +9,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numerical_methods as nm
 import time
-#scipy
+# scipy
 
 
 ###########################################################
 #Numerical solver for Dc'' = iwc, c'(0) = a, -D*c'(1) = j_hat#
 ###########################################################
 
-#Boundary Conditions and constants
+# Boundary Conditions and constants
 j_hat = 1
 D = 1
 a1 = 0
 a2 = -j_hat/D
 
+
 def get_matrix_problem(w, N):
-    #returns the matrix A and vector b for the matrix problem we need to solve
-    #Uses ghost points
-    second_derivative_matrix = np.diag(np.full(N+3, -2)) + np.diag(np.full(N+2, 1), -1) + np.diag(np.full(N+2, 1), 1)
+    # returns the matrix A and vector b for the matrix problem we need to solve
+    # Uses ghost points
+    second_derivative_matrix = np.diag(
+        np.full(N+3, -2)) + np.diag(np.full(N+2, 1), -1) + np.diag(np.full(N+2, 1), 1)
     A = D*second_derivative_matrix - w*1.j*np.identity(N+3)/N**2
 
-    #c_1 - c_-1 = 2*a1/N
-    #c_N+1 - c_N-1 = 2*a2/N
+    # c_1 - c_-1 = 2*a1/N
+    # c_N+1 - c_N-1 = 2*a2/N
     A[0][0] = -1
     A[0][1] = 0
     A[0][2] = 1
@@ -37,46 +39,51 @@ def get_matrix_problem(w, N):
     A[N+2][N+1] = 0
     A[N+2][N+2] = 1
 
-    b = [2*a1/N] + list(np.zeros(N+1)) + [2*a2/N]    
+    b = [2*a1/N] + list(np.zeros(N+1)) + [2*a2/N]
     return A, b
+
+
 def get_solutions(ws, method, N):
-    #gives answers for a list of frequencies
+    # gives answers for a list of frequencies
     answers = []
     start_timer = time.time()
     for w in ws:
         A, b = get_matrix_problem(w, N)
-        
-        #change the next line for different methods from alternate file
+
+        # change the next line for different methods from alternate file
         #c = np.linalg.solve(A, b)
         c = nm.solve(A, b, method)
-        #print(c)
+        # print(c)
         ans = c[N+1]/j_hat
-        
+
         answers.append(ans)
-        
+
     end_timer = time.time()
     time_taken = end_timer - start_timer
-    
+
     return answers, time_taken
 
+
 def get_full_solution_and_errors(w, method, N):
-    #Gives the full vector c for a specific frequency and gives errors
+    # Gives the full vector c for a specific frequency and gives errors
     A, b = get_matrix_problem(w, N)
-        
-    #change the next line for different methods from alternate file
+
+    # change the next line for different methods from alternate file
     #c = np.linalg.solve(A, b)
     c = nm.solve(A, b, method)
-    
-    #print(c)
+
+    print(c)
     c_exact = full_exact_sol(w, N)
+    print(c_exact)
     errors = np.abs(c[1:-1] - c_exact)
-    
+
     percentage_errors = 100*errors/np.abs(c_exact)
-    
+
     return c, percentage_errors
 
+
 def performance_vs_n(w, start_N, end_N, step_N, method):
-    #errors and times list has list of N, list of errors and list of times
+    # errors and times list has list of N, list of errors and list of times
     errors_and_times = [[], [], []]
     for n in range(start_N, end_N, step_N):
         answer, timer = get_solutions([w], method, n)
@@ -85,53 +92,60 @@ def performance_vs_n(w, start_N, end_N, step_N, method):
         errors_and_times[1].append(error)
         errors_and_times[2].append(timer)
     return errors_and_times
+
+
 def exact_sols(ws):
-    #get exact answers for the differential equation, takes list of frequencies
+    # get exact answers for the differential equation, takes list of frequencies
     answers = []
     for w in ws:
         y = np.sqrt(w/D*1.j)
         ans = - (1/(D*y))*(np.exp(y) + np.exp(-y))/(np.exp(y)-np.exp(-y))
         answers.append(ans)
-        
+
     return answers
 
+
 def full_exact_sol(w, N):
-    #get entire c vector for differential equation with a specific frequency
+    # get entire c vector for differential equation with a specific frequency
     answers = []
     for x in np.linspace(0, 1, N+1):
         y = np.sqrt(w/D*1.j)
         ans = - (1/(D*y))*(np.exp(y*x) + np.exp(-y*x))/(np.exp(y)-np.exp(-y))
         answers.append(ans)
-        
+
     return answers
 
+
 def get_errors_against_frequencies(frequencies, answers):
-    #gets errors from list of frequencies and corresponding answers
+    # gets errors from list of frequencies and corresponding answers
     exact_answers = np.array(exact_sols(frequencies))
     errors = np.abs(np.array(answers) - exact_answers)
-    
+
     percentage_errors = 100*errors/np.abs(exact_answers)
     mean_percentage_error = np.average(percentage_errors)
     return percentage_errors, mean_percentage_error
-#solve for range of w
+# solve for range of w
+
 
 def complex_plot(points):
-    #make a plot
+    # make a plot
     x = [point.real for point in points]
     y = [-point.imag for point in points]
-    
+
     # plot the numbers
     plt.scatter(x, y)
     plt.ylabel('-Imaginary')
     plt.xlabel('Real')
     plt.show()
 
+
 def plot_errors_against_frequencies(frequencies, errors):
     plt.scatter(frequencies, errors)
     plt.ylabel('Errors (%)')
     plt.xlabel('frequency')
     plt.show()
-    
+
+
 def plot_errors_against_x(errors):
 
     plt.scatter(np.linspace(0, 1, N+1), errors)
@@ -139,25 +153,27 @@ def plot_errors_against_x(errors):
     plt.xlabel('x')
     plt.show()
 
+
 def plot_errors_and_times_against_n(errors_and_times):
     fig, ax = plt.subplots()
-    
-    ax.plot(errors_and_times[0], errors_and_times[1], color = 'red')
-    ax.tick_params(axis = 'y', labelcolor = 'red')
+
+    ax.plot(errors_and_times[0], errors_and_times[1], color='red')
+    ax.tick_params(axis='y', labelcolor='red')
     ax2 = ax.twinx()
-    ax2.plot(errors_and_times[0], errors_and_times[2], color = 'green')
-    ax2.tick_params(axis = 'y', labelcolor = 'green')
+    ax2.plot(errors_and_times[0], errors_and_times[2], color='green')
+    ax2.tick_params(axis='y', labelcolor='green')
 
     ax.set_ylabel('error (%)')
     ax.set_xlabel('N')
     ax2.set_ylabel('time (s)')
     plt.show()
 
-#Number of steps 
+
+# Number of steps
 #N = int(input("Number of steps: "))+1
 N = 100
 
-#Choose solution method
+# Choose solution method
 method = 'standard'
 
 frequencies = range(1, 1000)
@@ -166,10 +182,10 @@ complex_plot(points)
 percentage_errors, mean_percentage_error = get_errors_against_frequencies(frequencies, points)
 plot_errors_against_frequencies(frequencies, percentage_errors)
 
-points, errors = get_full_solution_and_errors(1000, method, N)
+points, errors = get_full_solution_and_errors(1, method, N)
 plot_errors_against_x(errors)
 
 errors_and_times = performance_vs_n(1000, 50, 1000, 50, 'standard')
 plot_errors_and_times_against_n(errors_and_times)
 
-#print(timer)
+print(timer)
--- numerical_methods.py
+++ numerical_methods.py
@@ -16,6 +16,44 @@ def solve(A, b, method):
     return c
 
 def conjugate_gradient(A, b):
-    #TO DO
-    c = 0
-    return c
+    #choose x0 = 0
+    xk = np.zeros(np.shape(b))
+    rk = np.array(b)
+    pk = rk
+    
+    num_iter = 2
+    rk1rk1 = np.dot(rk, rk)
+    for k in range(num_iter):
+        Apk = A@pk
+        rkrk = rk1rk1
+        pkApk = np.dot(pk, Apk)
+        
+        alpha_k = rkrk / pkApk
+        xk = xk + alpha_k * pk
+        
+        rk = rk - alpha_k * Apk
+        
+        rk1rk1 = np.dot(rk, rk)
+        
+        beta_k = rk1rk1 / rkrk
+        
+        pk = rk + beta_k * pk
+
+    return xk
+
+'''
+A = np.array([[2, 100], [100, 2]])
+b = np.array([1, 5])
+c_exact = solve(A, b, 'standard')
+c = solve(A, b, 'cg')
+
+e = c - c_exact 
+
+print(c)
+print(c_exact)
+print(e)
+'''
+#CG required positive definite symmetric matrices 
+#Make matrix symmetric?
+#BiCG
+#BiCGSTAB?
