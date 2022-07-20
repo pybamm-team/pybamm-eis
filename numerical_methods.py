858e23d2b670b88478057601d8c2f2506becec90
rish31415 <rish31415@gmail.com>
2022-07-20 12:01:20 +0100

    Update numerical_methods.py
    Now also has unpreconditioned bicgstab

--- numerical_methods.py
+++ numerical_methods.py
@@ -8,10 +8,15 @@ import numpy as np
 
 def solve(A, b, method, start_point = 'none'):
     #This file will contain different algorithms to solve Ax = b
+    
+    if str(start_point) == 'none':
+        start_point = np.zeros(np.shape(b))
+        
+        
     if method == 'cg':
-        if str(start_point) == 'none':
-            start_point = np.zeros(np.shape(b))
         c = conjugate_gradient(A, b, start_point)
+    elif method == 'bicgstab':
+        c = bicgstab(A, b, start_point)   
     elif method == 'standard':
         c = np.linalg.solve(A, b)
     
@@ -23,7 +28,7 @@ def conjugate_gradient(A, b, start_point):
     rk = np.array(b) - A@xk
     pk = rk
     
-    max_num_iter = 10
+    max_num_iter = min(100, len(b))
     rk1rk1 = np.dot(np.conj(rk), rk)
     tol = 10**-5
     
@@ -49,13 +54,55 @@ def conjugate_gradient(A, b, start_point):
         
     return xk
 
+def bicgstab(A, b, start_point):
 
-A = np.array([[2, 10.j], [-10.j, 2]])
+    xk = np.array(start_point)
+    rk = np.array(b) - A@xk
+    r0 = np.conj(rk)
+    rhok = 1
+    alpha_k = 1
+    wk = 1
+    
+    pk = np.zeros(np.shape(b))
+    vk = pk
+    
+    max_num_iter = min(100, len(b))
+    tol = 10**-5
+    
+    for k in range(1, max_num_iter+1):
+        rhok1 = rhok
+        rhok = np.dot(r0, rk)
+        beta_k = (rhok/rhok1)*(alpha_k/wk)
+        
+        pk = rk + beta_k*(pk - wk*vk)
+        vk = A@pk
+        
+        alpha_k = rhok/np.dot(r0, vk)
+        
+        h = xk + alpha_k * pk
+        
+        if alpha_k*pk[-1] < tol:
+            xk = h
+            break
+        else:
+            s = rk - alpha_k*vk
+            t = A@s
+            
+            wk = np.dot(np.conj(t), s)/np.dot(np.conj(t), t)
+            
+            xk = h + wk*s
+            
+            rk = s - wk*t
+
+        
+    return xk
+
+A = np.array([[2+1.j, 1+3.j], [-10.j, 2]])
 b = np.array([1, 5])
 c_exact = solve(A, b, 'standard')
-c = solve(A, b, 'cg')
+c = solve(A, b, 'bicgstab')
 import scipy.sparse.linalg
-c2 = scipy.sparse.linalg.cg(A, b)
+c2 = scipy.sparse.linalg.bicgstab(A, b)
 
 
 e = c - c_exact 
