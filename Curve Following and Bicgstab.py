8b66dbd3b1b8a3a8f1d5bf872fe5872e211f1ef0
rish31415 <rish31415@gmail.com>
2022-07-20 11:25:16 +0100

    Tests different numerical methods
    Tests for large matrices. It seems Bicgstab is much better than cg for non-hermitian matrices.

--- Curve Following and Bicgstab.py
+++ Curve Following and Bicgstab.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul  4 12:11:11 2022
+Created on Mon Jul 18 15:51:34 2022
 
 @author: rish3
 """
 
+
 import numpy as np
 import matplotlib.pyplot as plt
-import numerical_methods as nm
 import time
-import scipy
 from scipy.sparse import csr_matrix
+import scipy.sparse.linalg
 
 ###########################################################
 #Numerical solver for Dc'' = iwc, c'(0) = a, -D*c'(1) = j_hat#
@@ -40,60 +40,29 @@ def get_matrix_problem(w, N):
     A[N+2][N+1] = 0
     A[N+2][N+2] = 1
 
+    A = csr_matrix(A)
     b = [2*a1/N] + list(np.zeros(N+1)) + [2*a2/N]
     return A, b
 
-def get_solutions(ws, method, N):
+def get_solutions(ws, N):
     # gives answers for a list of frequencies
     answers = []
     start_timer = time.time()
-    start_point = 'none'
+    start_point = np.zeros(N+3)
     for w in ws:
         A, b = get_matrix_problem(w, N)
         
         # change the next line for different methods from alternate file
         #c = np.linalg.solve(A, b)
-        c = nm.solve(A, b, method, start_point)
-        # print(c)
-        ans = c[N+1]/j_hat
+        c = scipy.sparse.linalg.bicgstab(A, b, x0 = start_point)
+        ans = c[0][N+1]/j_hat
 
         answers.append(ans)
-        start_point = c
+        start_point = c[0]
     end_timer = time.time()
     time_taken = end_timer - start_timer
 
     return answers, time_taken
-
-
-def get_full_solution_and_errors(w, method, N, start_point = 'none'):
-    # Gives the full vector c for a specific frequency and gives errors
-    A, b = get_matrix_problem(w, N)
-
-    # change the next line for different methods from alternate file
-    #c = np.linalg.solve(A, b)
-    c = nm.solve(A, b, method, start_point)
-
-    #print(c)
-    c_exact = full_exact_sol(w, N)
-    #print(c_exact)
-    errors = np.abs(c[1:-1] - c_exact)
-
-    percentage_errors = 100*errors/np.abs(c_exact)
-
-    return c, percentage_errors
-
-
-def performance_vs_n(w, start_N, end_N, step_N, method):
-    # errors and times list has list of N, list of errors and list of times
-    errors_and_times = [[], [], []]
-    for n in range(start_N, end_N, step_N):
-        answer, timer = get_solutions([w], method, n)
-        e, error = get_errors_against_frequencies([w], [answer])
-        errors_and_times[0].append(n)
-        errors_and_times[1].append(error)
-        errors_and_times[2].append(timer)
-    return errors_and_times
-
 
 def exact_sols(ws):
     # get exact answers for the differential equation, takes list of frequencies
@@ -105,18 +74,6 @@ def exact_sols(ws):
 
     return answers
 
-
-def full_exact_sol(w, N):
-    # get entire c vector for differential equation with a specific frequency
-    answers = []
-    for x in np.linspace(0, 1, N+1):
-        y = np.sqrt(w/D*1.j)
-        ans = - (1/(D*y))*(np.exp(y*x) + np.exp(-y*x))/(np.exp(y)-np.exp(-y))
-        answers.append(ans)
-
-    return answers
-
-
 def get_errors_against_frequencies(frequencies, answers):
     # gets errors from list of frequencies and corresponding answers
     exact_answers = np.array(exact_sols(frequencies))
@@ -125,8 +82,6 @@ def get_errors_against_frequencies(frequencies, answers):
     percentage_errors = 100*errors/np.abs(exact_answers)
     mean_percentage_error = np.average(percentage_errors)
     return percentage_errors, mean_percentage_error
-# solve for range of w
-
 
 def complex_plot(points):
     # make a plot
@@ -138,55 +93,23 @@ def complex_plot(points):
     plt.ylabel('-Imaginary')
     plt.xlabel('Real')
     plt.show()
-
+    
 
 def plot_errors_against_frequencies(frequencies, errors):
     plt.scatter(frequencies, errors)
     plt.ylabel('Errors (%)')
     plt.xlabel('frequency')
     plt.show()
-
-
-def plot_errors_against_x(errors):
-
-    plt.scatter(np.linspace(0, 1, N+1), errors)
-    plt.ylabel('Errors (%)')
-    plt.xlabel('x')
-    plt.show()
-
-
-def plot_errors_and_times_against_n(errors_and_times):
-    fig, ax = plt.subplots()
-    #Make this a log plot
-    ax.plot(errors_and_times[0], errors_and_times[1], color='red')
-    ax.tick_params(axis='y', labelcolor='red')
-    ax2 = ax.twinx()
-    ax2.plot(errors_and_times[0], errors_and_times[2], color='green')
-    ax2.tick_params(axis='y', labelcolor='green')
-
-    ax.set_ylabel('error (%)')
-    ax.set_xlabel('N')
-    ax2.set_ylabel('time (s)')
-    plt.show()
-
-
+    
 # Number of steps
 #N = int(input("Number of steps: "))+1
 N = 100
 
-# Choose solution method
-method = 'standard'
-
 frequencies = range(1, 1000)
-points, timer = get_solutions(frequencies, method, N)
+points, timer = get_solutions(frequencies, N)
 complex_plot(points)
 percentage_errors, mean_percentage_error = get_errors_against_frequencies(frequencies, points)
 plot_errors_against_frequencies(frequencies, percentage_errors)
-
-points, errors = get_full_solution_and_errors(100, method, N)
-plot_errors_against_x(errors)
-
-errors_and_times = performance_vs_n(1000, 50, 1000, 50, 'standard')
-plot_errors_and_times_against_n(errors_and_times)
-
 print(timer)
+
+
--- numerical_methods.py
+++ numerical_methods.py
@@ -6,53 +6,71 @@ Created on Tue Jul 12 16:10:36 2022
 """
 import numpy as np
 
-def solve(A, b, method):
+def solve(A, b, method, start_point = 'none'):
     #This file will contain different algorithms to solve Ax = b
     if method == 'cg':
-        c = conjugate_gradient(A, b)
+        if str(start_point) == 'none':
+            start_point = np.zeros(np.shape(b))
+        c = conjugate_gradient(A, b, start_point)
     elif method == 'standard':
         c = np.linalg.solve(A, b)
     
     return c
 
-def conjugate_gradient(A, b):
-    #choose x0 = 0
-    xk = np.zeros(np.shape(b))
-    rk = np.array(b)
+def conjugate_gradient(A, b, start_point):
+
+    xk = np.array(start_point)
+    rk = np.array(b) - A@xk
     pk = rk
     
-    num_iter = 2
-    rk1rk1 = np.dot(rk, rk)
-    for k in range(num_iter):
+    max_num_iter = 10
+    rk1rk1 = np.dot(np.conj(rk), rk)
+    tol = 10**-5
+    
+    for k in range(max_num_iter):
         Apk = A@pk
         rkrk = rk1rk1
-        pkApk = np.dot(pk, Apk)
+        pkApk = np.dot(np.conj(pk), Apk)
         
         alpha_k = rkrk / pkApk
+        
         xk = xk + alpha_k * pk
         
-        rk = rk - alpha_k * Apk
+        if alpha_k*pk[-1] < tol:
+            break
+        else:
+            rk = rk - alpha_k * Apk
+            
+            rk1rk1 = np.dot(np.conj(rk), rk)
+            
+            beta_k = rk1rk1 / rkrk
+            
+            pk = rk + beta_k * pk
         
-        rk1rk1 = np.dot(rk, rk)
-        
-        beta_k = rk1rk1 / rkrk
-        
-        pk = rk + beta_k * pk
-
     return xk
 
-'''
-A = np.array([[2, 100], [100, 2]])
+
+A = np.array([[2, 10.j], [-10.j, 2]])
 b = np.array([1, 5])
 c_exact = solve(A, b, 'standard')
 c = solve(A, b, 'cg')
+import scipy.sparse.linalg
+c2 = scipy.sparse.linalg.cg(A, b)
+
 
 e = c - c_exact 
+e2 = c2[0] - c_exact
 
 print(c)
 print(c_exact)
 print(e)
-'''
+
+print()
+print(c2)
+print(e2)
+
+
+
 #CG required positive definite symmetric matrices 
 #Make matrix symmetric?
 #BiCG
