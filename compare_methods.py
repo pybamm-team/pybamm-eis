fix impedance calculation

compare_methods.py
@@ -7,12 +7,11 @@ from plotting import nyquist_plot
 from scipy.fft import fft
 
 # Set up
-model = pybamm.lithium_ion.SPMe(options={"surface form": "differential"})
+model = pybamm.lithium_ion.SPM(options={"surface form": "differential"})
 
 parameter_values = pybamm.ParameterValues("Chen2020")
 
 frequencies = np.logspace(-4, 2, 30)
-# frequencies = [10]
 
 # Time domain
 I = 50 * 1e-3
@@ -45,7 +44,7 @@ for frequency in frequencies:
     voltage = sol["Terminal voltage [V]"].entries[-3 * samples_per_period - 1 :]
     # FFT
     current_fft = fft(current)
-    voltage_fft = fft(voltage - sol["Terminal voltage [V]"].entries[0])
+    voltage_fft = fft(voltage)
     # Get index of first harmonic
     idx = np.argmax(np.abs(current_fft))
     impedance = -voltage_fft[idx] / current_fft[idx]
@@ -59,8 +58,8 @@ for frequency in frequencies:
         ax[0, 1].plot(time, voltage)
         ax[1, 0].plot(x, np.abs(current_fft))
         ax[1, 1].plot(x, np.abs(voltage_fft))
-        ax[1, 0].set_xlim([0, frequency * 4])
-        ax[1, 1].set_xlim([0, frequency * 4])
+        ax[1, 0].set_xlim([0, frequency * 3])
+        ax[1, 1].set_xlim([0, frequency * 3])
         plt.show()
 
 end_time = timer.time()

eis_simulation.py
@@ -59,15 +59,16 @@ class EISSimulation:
         # Construct matrix problem
         solver = pybamm.BaseSolver()
         solver.set_up(self.built_model)
+        self.M = self.built_model.mass_matrix.entries
         self.y0 = self.built_model.concatenated_initial_conditions.entries
         self.J = self.built_model.jac_rhs_algebraic_eval(
             0, self.y0, []
         ).sparse()  # call the Jacobian and return a (sparse) matrix
-        self.M = self.built_model.mass_matrix.entries
         # Forcing on the current density variable, which is the
         # final entry by construction.
         self.b = np.zeros_like(self.y0)
         self.b[-1] = -1
+        # Store time and current scales
         self.timescale = self.built_model.timescale_eval
         self.current_scale = sim.parameter_values.evaluate(model.param.I_typ)
 